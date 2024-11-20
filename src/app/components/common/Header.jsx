d301a63a05b36d4be3e0226b418674b25f23791d
mj-saini <manojsaini97285@gmail.com>
2024-11-20 12:47:35 +0530

    remove opacity on desktop

--- src/app/components/common/Header.jsx
+++ src/app/components/common/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
           data-aos-offset="100"
           className={`lg:px-4 max-w-[1360px] mx-auto flex justify-between w-full items-center`}
         >
-        <div className="flex justify-between w-full lg:w-auto bg-[#FBFBFB] relative z-50 px-4 lg:px-0">
+        <div className="flex justify-between w-full lg:w-auto bg-[#FBFBFB] lg:bg-transparent relative z-50 lg:z-0 px-4 lg:px-0">
         <Link href="/" className=" ">
             <Image
               className={`w-[128px] sm:w-32 md:w-[160px] sm:py-3  py-[13px]`}
@@ -67,14 +67,14 @@ const Header = () => {
 
           <div
             className={`flex gap-5 xl:gap-8 max-lg:fixed left-0 max-lg:h-screen max-lg:w-screen max-lg:items-center max-lg:justify-start flex-col lg:!flex-row bg-white duration-500 z-10 ${
-              !toggleNav ? "!-top-[100vh] opacity-0" : "top-[63px] sm:top-[60px] md:top-[71px] px-5 pt-8 opacity-100"
+              !toggleNav ? "!-top-[100vh] opacity-0 lg:opacity-100" : "top-[63px] sm:top-[60px] md:top-[71px] px-5 pt-8 opacity-100"
             }`}
           >
             {headerData.map((tab, index) => (
               <div key={index} onClick={() => setToggleNav(false)}>
                 <Link
                   href={tab.path}
-                  className={`font-normal text-base capitalize flex items-center gap-3 justify-between duration-300 hover:text-black/60 ${
+                  className={`font-normal text-base capitalize flex items-center gap-3 justify-between duration-300 hover:text-black/60 z-10 ${
                     location === tab.path ? "text-black" : "text-[#292524]"
                   }`}
                 >
